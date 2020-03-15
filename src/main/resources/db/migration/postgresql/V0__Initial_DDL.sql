0fb7e1f8cb0aa299804e39165a977c0fd361598d
Nhan Vu <nhan997@gmail.com>
2020-03-15 21:33:25 +0100

    SQL initialize database

--- src/main/resources/db/migration/postgresql/V0__Initial_DDL.sql
+++ src/main/resources/db/migration/postgresql/V0__Initial_DDL.sql
@@ -1,12 +1,3 @@
--- CREATE TABLE account(
---    user_id serial PRIMARY KEY,
---    username VARCHAR (50) UNIQUE NOT NULL,
---    password VARCHAR (50) NOT NULL,
---    email VARCHAR (355) UNIQUE NOT NULL,
---    created_on TIMESTAMP NOT NULL,
---    last_login TIMESTAMP
--- );
-
 CREATE TABLE users (
     id serial PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
@@ -17,4 +8,27 @@ CREATE TABLE users (
     role VARCHAR(50) NOT NULL,
     created_on TIMESTAMP  NOT NULL
 );
+
+CREATE TABLE collections (
+  id BIGINT PRIMARY KEY,
+  topic VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  user_id BIGINT NOT NULL,
+  created_date DATE
+);
+
+CREATE TABLE images (
+  id BIGINT PRIMARY KEY,
+  url VARCHAR NOT NULL,
+  description VARCHAR,
+  user_id BIGINT NOT NULL,
+  collection_id bigint,
+  created_date DATE
+);
+
+ALTER TABLE collections ADD FOREIGN KEY (user_id) REFERENCES users (id);
+
+ALTER TABLE images ADD FOREIGN KEY (user_id) REFERENCES users (id);
+
+ALTER TABLE images ADD FOREIGN KEY (collection_id) REFERENCES collections (id);
 
