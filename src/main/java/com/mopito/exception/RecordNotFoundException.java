a026cc102d7db1991d3ee09ff928d51bfcdbb06d
Sang Nguyen <sang97@vt.edu>
2020-03-16 23:20:15 -0400

    add response status to exception files

--- src/main/java/com/mopito/exception/RecordNotFoundException.java
+++ src/main/java/com/mopito/exception/RecordNotFoundException.java
@@ -1,5 +1,9 @@
 package com.mopito.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class RecordNotFoundException extends RuntimeException {
     public RecordNotFoundException(String message) {
         super(message);
--- src/main/java/com/mopito/exception/UnauthorizedActionException.java
+++ src/main/java/com/mopito/exception/UnauthorizedActionException.java
@@ -1,5 +1,9 @@
 package com.mopito.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UnauthorizedActionException extends RuntimeException {
     public UnauthorizedActionException(String message) {
         super(message);
